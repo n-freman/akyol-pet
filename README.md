4425b15c14c3690e69dd2ed6c463bbc16965b931
n-freman <smiron1703@gmail.com>
2023-08-12 15:11:46 +0500

    add db config

---
+++ README.md
@@ -0,0 +1,8 @@
+# akyol PET
+
+### How to run
+
+- For the db service file to be readable from django
+```
+# export PGSERVICE='.pg_service.conf'
+```
--- core/settings.py
+++ core/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql",
+        "OPTIONS": {
+            "service": "my_service",
+            "passfile": ".my_pgpass",
+        },
     }
 }
 
@@ -116,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
