2fb5a86c763d64882be9bf6681ce73b8ffe254f3
n-freman <smiron1703@gmail.com>
2023-08-21 11:09:59 +0500

    added translations

--- apps/goods/models.py
+++ apps/goods/models.py
@@ -1,29 +1,54 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 from translations.models import Translatable
 
 
 class Preform(models.Model):
-    code = models.IntegerField(primary_key=True)
-    thread_standard = models.CharField(max_length=120)
-    weight = models.IntegerField()
-    height = models.FloatField(null=True)
-    blown_volume = models.FloatField()
-    image = models.ImageField(upload_to='preforms')
-    slug = models.SlugField()
+    code = models.IntegerField(_("code"), primary_key=True)
+    thread_standard = models.CharField(
+        _("Thread standard"),
+        max_length=120
+    )
+    weight = models.IntegerField(_("Weight"), )
+    height = models.FloatField(_("Height"), null=True)
+    blown_volume = models.FloatField(_("Blown volume"), )
+    image = models.ImageField(_("Image"), upload_to='preforms')
+    slug = models.SlugField(_("Slug"), )
 
     class Meta:
-        verbose_name = 'Preform'
-        verbose_name_plural = 'Preforms'
+        verbose_name = _('Preform')
+        verbose_name_plural = _('Preforms')
 
 
 class BottleCap(Translatable):
-    code = models.IntegerField(primary_key=True)
-    raw_materials = models.CharField(max_length=120)
-    application = models.CharField(max_length=255)
-    logo = models.CharField(max_length=255)
-    manufacturing_technology = models.CharField(max_length=120)
-    food_security = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='preforms')
+    code = models.IntegerField(
+        _("code"), 
+        primary_key=True
+    )
+    raw_materials = models.CharField(
+        _("Raw materials"), 
+        max_length=120
+    )
+    application = models.CharField(
+        _("Application"),
+         max_length=255
+    )
+    logo = models.CharField(
+        _("Logo"), 
+        max_length=255
+    )
+    manufacturing_technology = models.CharField(
+        _("Manufacturing technology"),
+        max_length=120
+    )
+    food_security = models.CharField(
+        _("Food security"),
+        max_length=255
+    )
+    image = models.ImageField(
+        _("Image"),
+        upload_to='preforms'
+    )
 
     class TranslatableMeta:
         fields = [
@@ -35,5 +60,5 @@ class BottleCap(Translatable):
         ]
 
     class Meta:
-        verbose_name = 'Bottle Cap'
-        verbose_name_plural = 'Bottle Caps'
+        verbose_name = _('Bottle Cap')
+        verbose_name_plural = _('Bottle Caps')
--- core/urls.py
+++ core/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path, re_path
-from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
